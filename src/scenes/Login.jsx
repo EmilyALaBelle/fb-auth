88b6bc65fa8423a23f7e6afbd238b11a909a46e7
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-08 11:37:27 -0500

    added sign in with email and password

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
+import { initializeApp } from "firebase/app"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
-export default function Login() {
+const firebaseConfig = {
+    apiKey: "AIzaSyCBn2IAmey6kZyb-_khAuiV6dW5I3POl_Y",
+    authDomain: "fb-auth-el.firebaseapp.com",
+    projectId: "fb-auth-el",
+    storageBucket: "fb-auth-el.appspot.com",
+    messagingSenderId: "256142708678",
+    appId: "1:256142708678:web:eca44052a9f92a128dacc7"
+};
+
+export default function Login({ setUser }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        const app = initializeApp(firebaseConfig) //connects to firebase
+        const auth = getAuth(app) //connects us to Firebase Auth
+        const response = await signInWithEmailAndPassword(auth, email, password)
+            .catch(alert)
+        setUser(response.user)
     }
     return (
         <>
